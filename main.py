f5aafe46480371a9accf67617bebfe6911628e2b
zoso9999 <fil.iacobelli@stud.uniroma3.it>
2023-02-10 16:44:33 +0100

    scegliconfigurazione

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 
 import pandas as pd
 import sys
+import random
 
 fin = open("input.xlsx", "rb")      # File di input
-#fout = open("output.txt", "w")     # File di output
-fout = sys.stdout                   # Output per deubg
+fout = open("output.txt", "w")     # File di output
+#fout = sys.stdout                   # Output per deubg
 header = None                       # Non è presente header nel file di input
 sname = "DatiOriginali"             # Nome del foglio da leggere
 nmacchine = 5                       # Numero di macchine del processo
-
+schedule = []
 
 
 def creaDizionario(nj,nc,nr):
@@ -20,7 +21,29 @@ def creaDizionario(nj,nc,nr):
         schedule = pd.read_excel(fin, header=header, sheet_name=sname, skiprows=skiprows, nrows=nmacchine, usecols=[col]).squeeze("columns").array
         id = ((i%20)+1)*100+col+1       # Per trovare una configurazione njob*100+nconfigurazione (njob ∈ [1,20] e ncofigurazione ∈ [1,40]
         dict[id]=schedule
+    print("Ho creato il dizionario delle configurazioni con successo")
     return dict
+
+
+def scegliConfigurazione(nc,dict,job,init):
+    global schedule
+    id = job*100+random.randint(1, nc)
+    config = dict[id]
+    if init:
+        schedule.append(id)
+    else:
+        min = job*100
+        max = (job+1)*100
+        for i in range(len(schedule)):
+            if schedule[i] >= min and schedule[i] < max:
+                if schedule[i] == job:
+                    return scegliConfigurazione(nc,dict,job,init)
+                else:
+                    schedule[i] = id
+                    break
+    return config
+
+
 
 
 #####################################################################################################################################
@@ -32,4 +55,9 @@ njob = int(nrighe/nmacchine)        # Numero job da eseguire
 nconfigrazioni = dimensions[1]      # Numero delle diverse configurazioni trovate
 
 d = creaDizionario(njob,nconfigrazioni,nrighe)
-print(d)
+for i in range(1,njob+1):
+    config = scegliConfigurazione(nconfigrazioni,d,i,True)
+print(schedule)
+config = scegliConfigurazione(nconfigrazioni,d,20,False)
+print(schedule)
+
